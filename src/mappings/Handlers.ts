22709e747fa42150128f528f58b1109917ec9779
CaiYiLiang <cherryliang0112@gmail.com>
2021-09-18 20:43:22 +1200

    upgrade pkg version

--- src/mappings/Handlers.ts
+++ src/mappings/Handlers.ts
@@ -2,27 +2,31 @@ import { SubstrateBlock } from '@subql/types';
 import { Staking } from '../types';
 
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
-  const { timestamp, block: rawBlock } = block;
-  const curEra = await api.query.staking.currentEra();
-  const stakingAmount = await api.query.staking.erasTotalStake(Number.parseInt(curEra.toString()));
-  const blockNum = rawBlock.header.number.toNumber();
+  try {
+    const { timestamp, block: rawBlock } = block;
+    const curEra = await api.query.staking.currentEra();
+    const stakingAmount = await api.query.staking.erasTotalStake(Number.parseInt(curEra.toString()));
+    const blockNum = rawBlock.header.number.toNumber();
 
-  const staking = await Staking.get(curEra.toString());
-  if (staking) {
-    staking.stakingAmount = stakingAmount.toString();
-    logger.info(` staking: ${staking.id} - stakingAmount: ${stakingAmount} `);
-    await staking.save();
-  } else {
-    logger.info(`======
-      CurERA: ${curEra} - stakingAmount: ${stakingAmount}
-      BlockNum: ${blockNum} - timestamp: ${timestamp}
-      ======`);
+    const staking = await Staking.get(curEra.toString());
+    if (staking) {
+      staking.stakingAmount = stakingAmount.toString();
+      logger.info(`BlockNum: ${blockNum} - Era: ${staking.id} - stakingAmount: ${stakingAmount} `);
+      await staking.save();
+    } else {
+      logger.info(`======
+        CurERA: ${curEra} - stakingAmount: ${stakingAmount}
+        BlockNum: ${blockNum} - timestamp: ${timestamp}
+        ======`);
 
-    await Staking.create({
-      id: curEra.toString(),
-      timestamp,
-      blockNum,
-      stakingAmount: stakingAmount.toString()
-    }).save();
+      await Staking.create({
+        id: curEra.toString(),
+        timestamp,
+        blockNum,
+        stakingAmount: stakingAmount.toString()
+      }).save();
+    }
+  } catch (err) {
+    logger.error(`handleBlock err: ${err} `);
   }
 }
